5fd833c994a587351b43d475b560db2d909a68be
case547 <justinmak2001@gmail.com>
2021-11-13 22:51:02 +0000

    Enable answering radio questions

--- main.py
+++ main.py
@@ -5,26 +5,35 @@ CHROMEDRIVER_LOC = '/Users/Justin/Downloads/chromedriver_win32/chromedriver'
 driver = Chrome(CHROMEDRIVER_LOC)
 driver.get('https://hdf.chp.gov.hk/dhehd/hdf-hkia.jsp?lang=en-us')
 
-passenger_data = [{ "surname_0":"Doe", "givenname_0": "Johnathan",
-                    "nationality_0": "Hong Kong", "hkid_0": "Y123456", "hkidcd_0": "7",
-                    "flightno_0": "CX 250", "seatno_0": "49K",
-                    "telephone_0": "98765432", "flat_0": "A", "floor_0": "69",
-                    "address_0": "420 Cool Road", "qaddress_0": "Empire Hotel Hong Kong"
-                  },
-                  {}
-                ]
+passenger_data = [
+    {
+        "surname_0":"Doe", "givenname_0": "Johnathan",
+        "birthyear_0": "1970", "birthmonth_0": "1", "birthday_0": "1",
+        "nationality_0": "Hong Kong", "hkid_0": "Y123456", "hkidcd_0": "7",
+        "flightno_0": "CX 250", "seatno_0": "49K",
+        "telephone_0": "98765432", "flat_0": "A", "floor_0": "69",
+        "address_0": "420 Cool Road"
+    },
+    {
+        "sex_0": "a", "hkResident_0": "a",
+        "aircrew_0": "b", "seacrew_0": "b", "otherExemptedPerson_0": "b",
+        "transitPort_0": "b", "isVisitedHighRiskCountry_0": "a",
+        "isChina_0": "b", "isForeign_0": "b",
+        "isVaccinatedBeforeHK_0": "a", "qAddressType_0": "b"
+    },
+    [
+        ["countryOriginal_0_1-button", "countryOriginal_0_1-menu", "211"],
+        ["countryCode_0-button", "countryCode_0-menu", "1"],
+        ["qDesignatedHotel_0-button", "qDesignatedHotel_0-menu", "13"]
+    ]
+]
 
 NEXT_BUTTON = '//*[@id="btnNext"]'
-
-# INPUT_SELECTOR = ".ui-input-text input"
-# RADIO_SELECTOR = ".ui-radio input"
-# DROPDOWN_SELECTOR = ".ui-select select"
-# CHECKBOX_SELECTOR = ".ui-checkbox input"
 
 DECLARATION_CHECK = '//*[@id="declareTC"]'
 COMPLETE_BUTTON = '//*[@id="goToFinal_0"]'
 
-driver.find_element_by_xpath(NEXT_BUTTON).click()
+driver.find_element(By.XPATH, NEXT_BUTTON).click()
 
 def answer_text(driver):
     """Answer all text-input questions"""
@@ -34,8 +43,23 @@ def answer_text(driver):
 
     return driver
 
-# def answer_radio(driver, ele):
-#     """Answer all radio button questions"""
-#     radio_qs = 
-#     for datum, question in zip(passenger_data[0], radio_qs):
-#         question.send_keys(datum)
+def answer_radio(driver):
+    """Answer all radio button questions"""
+    for key, val in passenger_data[1].items():
+        field = driver.find_element(By.ID, key)
+        field.find_element(By.CSS_SELECTOR, f"[for={key+val}]").click()
+
+    return driver
+
+# def answer_dropdowns(driver):
+#     "Answer all dropdown questions that require searching"
+#     for sublist in passenger_data[2]:
+#         driver.find_element(By.ID, sublist[0]).click()
+#         select_menu = driver.find_element(By.ID, sublist[1])
+#         select_menu.find_element(By.CSS_SELECTOR, f"[dataset.optionIndex={sublist[2]}]").click()
+
+#     return driver
+
+# driver = answer_text(driver)
+# driver = answer_radio(driver)
+driver = answer_dropdowns(driver)
